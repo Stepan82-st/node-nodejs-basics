92a215bc0560ff13ed8bfbc393dc933e9e6c2b89
Stepan Bryl <bryl.step@tut.by>
2022-12-17 20:12:14 +0300

    init copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import {ERROR_MSG, FILES_PATH} from './constants.js';
+import { exists, getAbsUrl } from './utils';
+
+const COPY_FOLDER_PATH = 'files_copy';
+const originalFolderUrl = getAbsUrl(FILES_PATH);
+const copyFolderUrl = getAbsUrl(COPY_FOLDER_PATH);
+
 const copy = async () => {
-    // Write your code here 
+
+if(await exists(copyFolderUrl) || !(await exists(originalFolderUrl))){
+    throw Error(ERROR_MSG);
+} else {
+    const [files] = await Promise.all([readdir(originalFolderUrl),mkdir(copyFolderUrl)]);
+    const promises = files.map((fileName) => 
+    copyFile(getAbsUrl(`$(FILES_PATH)/$(fileName)`), getAbsUrl(`$(COPY_FOLDER_PATH)/$(fileName)`)))
+    await Promise.all(promises);
+} // Write your code here 
 };
 
 copy();

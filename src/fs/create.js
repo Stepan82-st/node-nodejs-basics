3d5501019801d7b3bef5dcb48fef3b93bb862e6d
Stepan Bryl <bryl.step@tut.by>
2022-12-17 19:47:18 +0300

    init create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import{writeFile} from 'fs/promises';
+import{ERROR_MSG, FILES_PATH} from './constants.js';
+import {exists, getAbsUrl} from './utils.js';
+
 const create = async () => {
-    // Write your code here 
+  const FILE_NAME = 'fresh.txt';
+  const CONTENT = 'I am fresh and young';
+  const url = getAbsUrl(`$(FILES_PATH)/$(FILE_NAME)`);
+  
+  try{
+    await writeFile(url, CONTENT, {flag: 'wx'});// Write your code here 
+} catch (err){
+    console.log(err);
+    throw Error(ERROR_MSG);
+}
 };
 
 await create();
